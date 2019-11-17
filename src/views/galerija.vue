c5c20769546d7865bc3cdb9e64260aa8b0c5f563
ajurman <56589117+ajurman@users.noreply.github.com>
2019-11-17 23:04:45 +0100

    Create galerija.vue

---
+++ src/views/galerija.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Galerija</h1>
+    <div class="container tabs is-large">
+      <ul>
+        <li class="is-active"><a>2019</a></li>
+        <li><a>2018</a></li>
+        <li><a>2017</a></li>
+        <li><a>2016</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+body.home { 
+  background-image: url("https://www.croatiaweek.com/wp-content/uploads/2019/10/04.jpg") ;
+}
+</style>
