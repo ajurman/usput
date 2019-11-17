3dc5dd9cfa08ec1f66566949d33e51dcf466637f
ajurman <56589117+ajurman@users.noreply.github.com>
2019-11-17 17:42:43 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium">      
+      <nav class="tabs is-centered" aria-label="breadcrumbs">
+        <ul>
+          <li><router-link to="/">Početna</router-link></li>
+          <li><router-link to="/o-nama">Galerija</router-link></li>
+          <li><a href="#">Lokacija</a></li>
+          <li ><a href="#">Kontakt</a></li>
+        </ul>
+      </nav>
+    </section>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+body.home { 
+        background-image: url("https://ivijesti.hr/wp-content/uploads/photo-gallery/Internacionalni_sajam_pr%C5%A1uta_u_Tinjanu/06.jpg?bwg=1571716807") ;
+        background-position: center center;
+        background-repeat:  no-repeat;
+        background-attachment: fixed;
+        background-size:  cover;
+        background-color: #999;
+    }
+nav {
+  margin-top: 20px;
+}  
 
 
+nav li {
+  font-weight: bold;
+  color: #000000;
+}
+
+nav a.router-link-exact-active {
+  color: #4254b9;
+}
+</style>
