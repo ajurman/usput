c9977e201642b7e2a202e273600c779841a676a6
ajurman <56589117+ajurman@users.noreply.github.com>
2019-11-17 17:43:01 +0100

    Create pocetna.vue

---
+++ src/views/pocetna.vue
@@ -0,0 +1,28 @@
+<template>
+        <article class="hero-body has-background-dark">
+            <div class="container">
+                <h1 class="title has-text-white-ter has-text-centered">Stranica</h1>
+                <h2 class="subtitle has-text-centered">Podnaslov</h2>
+
+                <div class="columns is-mobile">
+                    <div class="column is-half is-offset-one-quarter has-background-light"></div>
+                </div>
+            </div>
+        </article>
+</template>
+
+<script>
+export default {
+  name: "Pocetna",
+  beforeCreate: function() {
+        document.body.className = 'home';
+    }
+}
+</script>
+
+<style scoped>
+    article {
+        margin-top: 800px;
+    }
+    
+</style>
