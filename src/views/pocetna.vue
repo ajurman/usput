78a1e88500709989f0ec08958e3b3f552e8da596
ajurman <56589117+ajurman@users.noreply.github.com>
2019-11-17 23:04:22 +0100

    Update pocetna.vue

--- src/views/pocetna.vue
+++ src/views/pocetna.vue
@@ -1,14 +1,61 @@
 <template>
-        <article class="hero-body has-background-dark">
-            <div class="container">
-                <h1 class="title has-text-white-ter has-text-centered">Stranica</h1>
-                <h2 class="subtitle has-text-centered">Podnaslov</h2>
-
-                <div class="columns is-mobile">
-                    <div class="column is-half is-offset-one-quarter has-background-light"></div>
+    <div>
+        <article class="hero is-large">
+            <div class="hero-body">
+                <div class="container">
+                    <h2 class="subtitle has-text-info">
+                    Primary bold subtitle
+                    </h2>
+                    <h1 class="title  has-text-light">
+                        Primary bold title
+                    </h1>
                 </div>
             </div>
         </article>
+        <article class="hero-body has-background-white-ter">
+            <div class="container">
+                <h3 class="title">Internacionalni sajam pršuta</h3>
+                <div class="columns is-mobile">
+                    <div class="column">
+                        <div class="notification is-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quasi perferendis culpa atque sed voluptatibus unde animi laudantium deleniti ipsam totam doloribus qui architecto, nostrum provident quaerat odio! Doloribus, consequatur?</div>
+                        <div class="notification is-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quasi perferendis culpa atque sed voluptatibus unde animi laudantium deleniti ipsam totam doloribus qui architecto, nostrum provident quaerat odio! Doloribus, consequatur?</div>
+                        <div class="notification is-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quasi perferendis culpa atque sed voluptatibus unde animi laudantium deleniti ipsam totam doloribus qui architecto, nostrum provident quaerat odio! Doloribus, consequatur?</div>
+                        <div class="notification is-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quasi perferendis culpa atque sed voluptatibus unde animi laudantium deleniti ipsam totam doloribus qui architecto, nostrum provident quaerat odio! Doloribus, consequatur?</div>
+                        <figure class="image">
+                            <img src="https://erickson.edu/sites/default/files/Erickson%20Badges%20Banner.png">
+                        </figure>
+                    </div>
+                </div>
+                <div class="has-background-info">
+                    <h4 class="has-background-white">SPONZORI</h4>
+                </div>
+                <div class="columns is-mobile">
+                         <div class="column">1</div>
+                        <div class="column">2</div>
+                        <div class="column">3</div>
+                        <div class="column">4</div> 
+                </div>
+                <div class="columns is-mobile">
+                         <div class="column">1</div>
+                        <div class="column">2</div>
+                        <div class="column">3</div>
+                        <div class="column">4</div> 
+                </div>
+                <div class="columns is-mobile">
+                        <div class="column">1</div>
+                        <div class="column">2</div>
+                        <div class="column">3</div>
+                        <div class="column">4</div> 
+                </div>
+                <div class="columns is-mobile">
+                         <div class="column">1</div>
+                        <div class="column">2</div>
+                        <div class="column">3</div>
+                        <div class="column">4</div> 
+                </div>
+            </div>
+        </article>
+    </div>
 </template>
 
 <script>
@@ -21,8 +68,21 @@ export default {
 </script>
 
 <style scoped>
-    article {
-        margin-top: 800px;
+    article:nth-child(1) {
+        text-align: left;
+        padding-top: -100px;
+
+    }
+    article:nth-child(2) {
+        margin-top: 300px;
+    }
+    hr {    padding-bottom: 80px;}
+    h3,.column>p {
+        padding: 25px;
+    
+    }
+    h4 {
+        display:inline-block;
     }
     
 </style>
