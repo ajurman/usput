fac2b1c9e62a8f0d48eaaa581b307765842c6a38
ajurman <56589117+ajurman@users.noreply.github.com>
2019-12-26 11:31:43 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <template>
   <div id="app">
-    <div class="tamnija">
-      <nav class="tabs is-centered is-size-4" aria-label="breadcrumbs">
-            <ul>
-              <li><router-link to="/"  class="has-text-white">Početna</router-link></li>
-              <li><router-link to="/galerija"  class="has-text-white">Galerija</router-link></li>
-              <li><router-link to="/lokacija"  class="has-text-white">Lokacija</router-link></li>
-              <li ><router-link to="/kontakt"  class="has-text-white">Kontakt</router-link></li>
-            </ul>
-      </nav>
-      <div class="ssnap">
-      </div>
-        <router-view/>
-        <section class="footer is-medium tamnija">
-          <div class="container">
-            <div class="columns">
-              <div class="column has-text-left redText">
-                <p>Adresa</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-              </div>
-              <div class="column">
-              </div>
-              <div class="column">
-              </div>
-              <div class="column has-text-right redText">
-                <p>Kontakt</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <button>social media</button>
-              </div>
+    <nav 
+      v-if="['galerija', 'lokacija', 'kontakt'].indexOf($route.name) > -1" 
+      class="tabs is-centered is-size-4" aria-label="breadcrumbs">
+        <ul>
+      <li><router-link to="/"  class="has-text-white">Početna</router-link></li>
+      <li><router-link to="/galerija"  class="has-text-white">Galerija</router-link></li>
+      <li><router-link to="/lokacija"  class="has-text-white">Lokacija</router-link></li>
+      <li ><router-link to="/kontakt"  class="has-text-white">Kontakt</router-link></li>
+        </ul>
+    </nav>
+      <router-view/>
+      <section class="footer is-medium">
+        <div class="container">
+          <div class="columns">
+            <div class="column has-text-left redText">
+              <p>Adresa</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
-            <hr>
-            copyright
+            <div class="column">
+            </div>
+            <div class="column">
+            </div>
+            <div class="column has-text-right redText">
+              <p>Kontakt</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <button>social media</button>
+            </div>
           </div>
-        </section>
-      </div>
+          <hr>
+          copyright
+        </div>
+      </section>
     </div>
 </template>
 
@@ -41,3 +39,9 @@
 @import "assets/css/main.scss";
 
 </style>
+<script>
+export default {
+  methods: {
+  }
+}
+</script>
