ccb80c5e8c409bace001a6398e33fccb5b788807
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-08 10:26:37 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
+
 import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import VModal from 'vue-js-modal'
+
+Vue.use(VModal, { dynamic: true, injectModalsContainer: true })
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCA53dTtqh9bPm2nRjbl8au5yqxE4-F4DI'
+  }
+})
 
 Vue.use(Buefy)
-
 Vue.config.productionTip = false
 
 new Vue({
