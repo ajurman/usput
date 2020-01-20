db52aa7ea828424cbc0bcfa4e582ecf49e5c8355
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-20 13:57:35 +0100

    Update lokacija.vue

--- src/views/lokacija.vue
+++ src/views/lokacija.vue
@@ -1,24 +1,39 @@
 <template>
     <div>
-        <div>
-            <h1>Google Maps Demo</h1>
-
-            <GmapMap
-            :center="center"
-            :zoom="9"
-            >
-            <GmapMarker
-                :key="index"
-                v-for="(m, index) in markers"
-                :position="m.position"
-                :clickable="true"
-            />
-            </GmapMap>
-        </div>
-        <div>
-            <h2>O općini</h2>
-            <p>Općina Tinjan nalazi se u centralnom dijelu središnje Istre, na prostoru zapadne strane Drage.Tinjan ponosno čuva svoju povijest i tradiciju, bilo da je riječ o simbolima ovog kraja, kamenom suhozidu i lokvama te kosirima i rankunima, folklornom i graditeljskom nasljeđu, predaji i legendama ili pak o vrhunskim gastronomskim delicijama poput istarskog pršuta. Za očuvanje tradicije proizvodnje vrhunskog pršuta na Tinjanštini danas je zaslužno nekoliko registriranih pršutana, a 2006. godine Tinjan je proglašen Općinom istarskog pršuta. </p>
-            
+        <section class="slika"></section>
+            <div>
+                <div class="container lokacija-tekst">
+                <h1 class="title">
+                    Tinjan
+                </h1>
+                <h5>
+                    Nalazi se u centralnom dijelu središnje Istre, na prostoru zapadne strane Drage.Tinjan ponosno čuva svoju povijest i tradiciju, bilo da je riječ o simbolima ovog kraja, kamenom suhozidu i lokvama te kosirima i rankunima, folklornom i graditeljskom nasljeđu, predaji i legendama ili pak o vrhunskim gastronomskim delicijama poput istarskog pršuta. Za očuvanje tradicije proizvodnje vrhunskog pršuta na Tinjanštini danas je zaslužno nekoliko registriranih pršutana, a 2006. godine Tinjan je proglašen Općinom istarskog pršuta.
+                </h5>
+                </div>
+            </div>
+        <div class="container razmak">
+            <div class="columns is-gapless has-background-light">
+                <div class="column">
+                    <GmapMap
+                    :center="center"
+                    :zoom="9"
+                    >
+                    <GmapMarker
+                        :key="index"
+                        v-for="(m, index) in markers"
+                        :position="m.position"
+                        :clickable="true"
+                    />
+                    </GmapMap>
+                </div>
+                <div class="column is-4">
+                    <div class="crta-siva"></div>
+                    <i class="mdi mdi-google-maps "></i>
+                    <h6 class="is-size-3">Lokacija</h6>
+                    <p><b>lat</b> 45.2176</p>
+                    <p><b>lng</b> 13.8369 </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -37,6 +52,61 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+
+.slika {
+        width: 100%;
+        height:50vh;
+    }
+.razmak{
+        margin: 5rem auto;
+    }
+$siva : rgb(97, 97, 97);
+.razmak{
+    margin-bottom: 3rem;
+}
+section {
+    width: 100%;
+
+}
+.slika {
+    background-image: url("../assets/JPEG/Tinjan.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+h5, h1 {
+    padding-top:1rem;
+    line-height: 30px;
+    color:rgb(228, 228, 228);
+}
+.crta-siva{
+    height:50px;
+    background-color:$siva;
+
+}
+.lokacija-tekst{
+    min-height: 30vh;
+    z-index: 10;
+    margin-top: -100px;
+    padding:3rem !important;
+    background-color:$siva;
+}
+.is-4 {
+    color:$siva;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    p {
+        padding:1rem;
+    }
+    h6 {
+        padding: 1.5rem;
+    }
+    i {
+        padding-top: 1rem;
+    }
+}
 .vue-map-container {
   height: 450px;
   max-width: 992px;
