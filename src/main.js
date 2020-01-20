2e7401baa6a24219aeaea0ad2f9b617be80fce68
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-20 13:56:51 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
 import * as VueGoogleMaps from 'vue2-google-maps'
-import VModal from 'vue-js-modal'
 
-Vue.use(VModal, { dynamic: true, injectModalsContainer: true })
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyCA53dTtqh9bPm2nRjbl8au5yqxE4-F4DI'
