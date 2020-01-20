438f68e17d5e782935cc261e56263b09aa9a29d4
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-20 13:56:46 +0100

    Create sponzori.vue

---
+++ src/components/sponzori.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="sponzori white">
+    <div class="container has-background-white">
+        <h4 class="has-text-black has-text-centered">SPONZORI</h4>
+        <div class="columns is-vertical-center is-mobile">
+                <div class="column"><img src="../assets/JPEG/sponzor1.jpg" alt="GhiaStaff"></div>
+                <div class="column"><img src="../assets/JPEG/sponzor2.jpg" alt="Rudan"></div>
+        </div>
+        <h4 class="has-text-black has-text-centered">POKROVITELJI I POTPORE</h4>
+        <div class="columns is-mobile is-vertical-center">
+                <div class="column"><img src="../assets/JPEG/potpore2.jpg" alt="Ministarstco turizma"> </div>
+                <div class="column"><img src="../assets/JPEG/potpore3.jpg" alt="Ministarstvo poljoprivrede"></div>
+        </div>
+        <div class="columns is-mobile drugi is-vertical-center">
+            <div class="column"><img src="../assets/JPEG/potpore5.jpg" alt="Središnja Istra"></div>
+            <div class="column"><img src="../assets/JPEG/potpore1.jpg" alt="Istra"></div>
+            <div class="column"><img src="../assets/JPEG/potpore4.jpg" alt="Istra"></div>
+        </div>
+        <h4 class="has-text-black has-text-centered">ORGANIZATORI</h4>
+        <div class="columns is-mobile is-vertical-center">
+            <div class="column"><img src="../assets/JPEG/organizator1.jpg" alt="GUSTUS"></div> 
+            <div class="column"><img src="../assets/JPEG/potpore6.jpg" alt="Središnja Istra"></div>    
+            <div class="column"><img src="../assets/JPEG/organizator2.jpg" alt="Tinjan"></div>
+        </div>
+        <div class="columns is-mobile drugi is-vertical-center">
+            <div class="column"><img src="../assets/JPEG/organizator3.jpg" alt="Istarski Pršut"></div>
+            <div class="column"><img src="../assets/JPEG/organizator4.jpg" alt="Merikane"></div>  
+        </div>
+    </div>                    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sponzori img {
+    display: block;
+    margin: auto;
+}
+.sponzori{
+    padding-top: 3rem;
+}
+.sponzori div {
+    border-radius: 20px !important;
+}
+</style>
