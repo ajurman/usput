6d45a3ca0b82ce15cdf33b3f560bdf4c9e299214
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-24 16:41:22 +0100

    Update brojke.vue

--- src/components/brojke.vue
+++ src/components/brojke.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="brojke">
-    <div class="container has-text-centered">
+    <!--<div class="container has-text-centered">
         <div
         v-for="(item,indeks) in items"
         :key="item.klasa"
@@ -14,12 +14,51 @@
                 </div>
                 
             </div>
-            <div class="column desni centar"
+            <div class="column desni centar is-half"
             :class="{'margine': (indeks %2)!=0}">
                 <div>
                     <p class="opis-brojki is-size-6">{{item.tekst}}</p>
                 </div>
             </div>                      
+        </div>
+    </div>-->
+    <div class="container">
+        <div class="columns">
+            <div 
+                class="card column"
+                v-for="(item) in items"
+                :key="item.klasa">
+                <div class="card-content">
+                    
+                   <!-- <div class="media-left">
+                        <b-icon
+                            icon="format-list-numbered"
+                            size="is-large"
+                            type="is-success">
+                        </b-icon>
+                    </div>
+                    <div class="media-content">
+                        {{item.title}}
+                        {{item.heading}}
+                    </div>-->
+                    <div class="card-naslov">
+                        <b-icon
+                            :icon="item.klasa"
+                            size="is-large">
+                        </b-icon>
+                        <span id="pomak">
+                        <b>{{item.title}}
+                        {{item.heading}}</b>
+                        </span>
+                        
+                    </div>
+                    <hr>
+
+                    <div class="content is-size-7">
+                    {{item.tekst}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -31,29 +70,29 @@ export default {
             istina: true,
             items:[ 
                 {
-                    klasa:"mdi-format-list-numbered",
+                    klasa:"format-list-numbered",
                     heading:"Po redu",
                     title:"13.",
-                    tekst:"Internacionalni sajam pršuta ISAP se od 2006. godine, kada se Općina Tinjan proglasila Općinom istarskog pršuta, redovno održava u Tinjanu trećeg vikenda u listopadu.Manifestacija je to koja je doprinijela prepoznatljivosti Općine i cijele ruralne Istre."
+                    tekst:"Internacionalni sajam pršuta ISAP se od 2006. godine, kada se Općina Tinjan proglasila Općinom istarskog pršuta, redovno održava u Tinjanu trećeg vikenda u listopadu"
                 },
                 {
-                    klasa:"mdi-account-group",
+                    klasa:"account-group",
                     heading:"Posjetitelja",
                     title:"30.000+",
                     tekst:"Posjetitelji iz svih krajeva Hrvatske i Europe, ali i s drugih kontinenata, oslobode tri dana za odlazak u Tinjan, opisavši pritom ovu sjajnu manifestaciju kao nešto što se čeka cijelu godinu, i što se nikako ne propušta"
                 },
                 {
-                    klasa:"mdi-account-badge",
+                    klasa:"account-badge",
                     heading:"Natjecatelja",
                     title:"81",
-                    tekst:"Svi izlagači, vrsni majstori svog zanata, na stol su stavili najbolje iz svoje ponude, pokazavši tako razlike u finesama spravljanja pršuta, kralja delicija. Svake godine se najuspješnijim pršutarima dodjeljuju plakete i priznanja za kvalitetu njihovih proizvoda te se proglašava i šampion sajma"
-                },
+                    tekst:"Svi izlagači, vrsni majstori svog zanata, na stol su stavili najbolje iz svoje ponude, pokazavši tako razlike u finesama spravljanja pršuta, kralja delicija."
+                }/*,
                 {
                     klasa:"mdi-weight-kilogram",
                     heading:"Pršuta pojedeno",
                     title:"20.000+ kg",
                     tekst:"Ovo je najbolji pokazatelj sve veće popularnosti događaja. U posljednih 7 godine se ukupna količina pojedenih pršuta povećala za skoro ČETIRI puta. Gastro ponuda se ne sastoji samo od pršuta i suhomesnatih proizvoda, već i od niz drugih gastro proizvoda (sir, maslinovo ulje, vino, voće te proizvodi od voća i povrća)"
-                }
+                }*/
                 
             ]
         }
@@ -66,8 +105,5 @@ export default {
 </script>
 
 <style>
-
-</style>
-<style lang="scss" scoped>
 
 </style>
