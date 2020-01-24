592268d2ee15b9e3f84faaccc4f09617fc142b0f
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-24 16:41:50 +0100

    Update pocetna.vue

--- src/views/pocetna.vue
+++ src/views/pocetna.vue
@@ -3,35 +3,35 @@
         <header>
             <div class="navbar-brand">
                 <router-link to="/" class="navbar-item">
-                <img src="logo.png" alt="logo">
+                    <img src="../assets/logo.png" alt="logo">
                 </router-link>
             </div>
-            <nav class="tabs is-centered is-size-4" aria-label="breadcrumbs">
+            <nav class="navic tabs is-centered" aria-label="breadcrumbs">
                     <ul>
-                    <li><router-link to="/"  class="has-text-white is-size-4 nav-link">Početna</router-link></li>
-                    <li><router-link to="/galerija"  class="has-text-white is-size-4 nav-link">Galerija</router-link></li>
-                    <li><router-link to="/lokacija"  class="has-text-white is-size-4 nav-link">Lokacija</router-link></li>
-                    <li ><router-link to="/kontakt"  class="has-text-white is-size-4 nav-link">Kontakt</router-link></li>
+                    <li><router-link to="/"  class="has-text-white is-size-6 nav-link">Početna</router-link></li>
+                    <li><router-link to="/galerija"  class="has-text-white is-size-6 nav-link">Galerija</router-link></li>
+                    <li><router-link to="/lokacija"  class="has-text-white is-size-6 nav-link">Lokacija</router-link></li>
+                    <li ><router-link to="/kontakt"  class="has-text-white is-size-6 nav-link">Kontakt</router-link></li>
                     </ul>
                 </nav>
             <div class="navbar-cta">
                 <router-link to="/lokacija">
-                    <button class="cta button is-medium">POSJETI ISAP</button>
+                    <button class="cta button">POSJETI ISAP</button>
                 </router-link>
             </div>
             <div class="header"> 
-                <img src="../assets/pozadina.jpg" id="pozadina-slika">
+               
                 <div id="overlay"></div>
                 <div id="naslov">
-                    <h1 >PRŠUT – ZAŠTITNI ZNAK OPĆINE TINJAN</h1>
-                    <h2 class="has-text-white">GASTRONOMSKU TRADICIJU PRETVORILI SMO U NAŠIROKO POZNAT DOGAĐAJ</h2>
+                    <h1 class="is-size-3" >PRŠUT – ZAŠTITNI ZNAK OPĆINE TINJAN</h1>
+                    <h2 class="has-text-white is-size-6">GASTRONOMSKU TRADICIJU PRETVORILI SMO U NAŠIROKO POZNAT DOGAĐAJ</h2>
                 </div>
                 <video autoplay loop muted class="video-background" >
                     <source src="../assets/trimmed.mp4" type="video/mp4">
                 </video>
                 <div class="crtica"></div>
             </div>
-            <div id="bocno">
+            <div id="bocno" class="is-size-4">
                 <span>TINJAN | 18.-20.10.2019.</span>
                 <a href="" class="redText">FB</a>
                 <a href="" class="redText">IG</a>
@@ -47,7 +47,7 @@
                             <div class="vh-50 slika-text">
                                 <div class="text-slika">
                                     <h4>Što je ISAP?</h4><br>
-                                    <button class="button cta is-large" :class="{'is-invisible-cta':showCta }" @click="show = !show; prvaSlika(); showCta = !showCta">Saznaj više</button>
+                                    <button class="button cta" :class="{'is-invisible-cta':showCta }" @click="show = !show; prvaSlika(); showCta = !showCta">Saznaj više</button>
 
                                 </div>
                             </div>
@@ -56,7 +56,7 @@
                             <div class="vh-50 slika-text">
                                 <div class="text-slika">
                                     <h4>Zašto pršut?</h4><br>
-                                    <button class="button cta is-large" :class="{'is-invisible-cta':showCta }" @click="show = !show; drugaSlika(); showCta = !showCta">Saznaj više</button>
+                                    <button class="button cta" :class="{'is-invisible-cta':showCta }" @click="show = !show; drugaSlika(); showCta = !showCta">Saznaj više</button>
                                 </div>
                             </div>
                         </div>        
@@ -70,12 +70,12 @@
 
 
                     <div class="probajte">
-                        <h4 id="probajte">I Vi zaslužujete uživati</h4>
+                        <h4 id="probajte" class="is-size-1">I Vi zaslužujete uživati</h4>
                         <div id="gradient"></div>
                         <div class="v-centar">
                             <br>
                             <router-link to="/lokacija">
-                                <button class="cta button is-medium" :class="{'is-invisible-cta':showCta }">POSJETI ISAP</button>
+                                <button class="cta button " :class="{'is-invisible-cta':showCta }">POSJETI ISAP</button>
                             </router-link>
                         </div>
                         
@@ -100,14 +100,13 @@
                 
             <div class="columns">
                 <div class="column">
-                <div class="is-size-4 is-size-6-mobile">
+                <div>
                     <p>
                     {{text1}}<br><br><br>
                     {{text2}}<br><br><br>
-                    {{text3}}<br><br><br>
                     </p>
                     <router-link to="/lokacija">
-                    <button class="is-medium cta button" >POSJETI ISAP</button>
+                    <button class=" cta button" >POSJETI ISAP</button>
                     </router-link>
                 </div>
                 </div></div>
@@ -143,14 +142,12 @@ export default {
         prvaSlika(){
         this.text1= "Internacionalni sajam pršuta ISAP se od 2006. godine, kada se Općina Tinjan proglasila Općinom istarskog pršuta, redovno održava u Tinjanu trećeg vikenda u listopadu. Jedna je od najvažnijih turističkih i gospodarskih manifestacija središnje Istre koju godišnje posjeti više od 25.000 ljudi.";
         this.text2= "Svake godine se najuspješnijim pršutarima dodjeljuju plakete i priznanja za kvalitetu njihovih proizvoda te se proglašava i šampion sajma, a sve temeljem ocjene stručne komisije koju čine senzorni analitičari pršuta, članovi Udruge senzornih analitičara pršuta - GUSTUS osnovane u Tinjanu 2010. godine. ";
-        this.text3="Specifičnost gastronomske ponude manifestacije ISAP ogleda se u mogućnosti da se u isto vrijeme na jednom mjestu nađu pršuti iz svih krajeva svijeta i da se mogu kušati, uspoređivati i valorizirati.  Osim gastro ponude, koja se ne sastoji samo od pršuta i suhomesnatih proizvoda kao najvažnijeg dijela manifestacije, već i od niz drugih gastro proizvoda (sir, maslinovo ulje, vino, voće te proizvodi od voća i povrća, suveniri…) koji se nude na mnogobrojnim štandovima tijekom ISAP-a u Tinjanu.  Nezaobilazni zabavni sadržaj ISAP-a je i organizacija mnogobrojnih koncerata poznatih glazbenih izvođača sa hrvatskog područja i izvan njega.";
         this.naslov="Što je ISAP?";
         this.slika=require("../assets/JPEG/isap.jpg") ;
         },
         drugaSlika(){
         this.text1= "Istarski pršut je trajni suhomesnati proizvod, obrađen na istarski način sa i bez zdjeličnih kosti, suho salamuren morskom soli i prirodnim mirodijama, sušen na zraku i bez dimljenja.";
         this.text2= "Prirodni uvjeti Istre Istarski pršut se najviše proizvodi u unutrašnjosti istarskog poluotoka, gdje klimatski uvjeti omogućuju soljenje mesa i proizvodnju pršuta samo tijekom zimskih mjeseci. Dobar pokazatelj klimatskih prilika kakve vladaju u središnjem dijelu Istre služi šire područje Pazina. Prema podacima Državnog Hidrometeorološkog Zavoda, srednje vrijednosti temperatura i relativne vlage zraka od prosinca do ožujka pogoduju optimalnom prirodnom sušenju mesa. Početna niska temperatura(3-4 stupnja Celzija) i postepen pad relativne vlažnosti zraka(65-70%), omogućavaju pravilan proces sušenja i zrenja pršuta. Dolaskom proljeća i toplijih dana, pršuti se premještaju na zrenje u podrumske prostorije gdje je sada optimalna temperatura zraka 13-15 stupnjeva Celzija i stabilna vlažnost zraka od 65-70 %. Istarsku klimu karakteriziraju vjetrovi koji pogoduju sušenju pršuta. Prema podacima Državnog Hidrometeorološkog Zavoda u središnjem dijelu Istre u razdoblju l989 – l998 godine je bilo svega 11% dana bez vjetra. U središnjem dijelu Istre najčešće puše vjetar sa istoka i jugoistoka te južni vjetrovi. Za pravilno sušenje i zrenje pršuta te stvaranje prirodnih uvjeta, najzaslužniji su vjetrovi sjevernog i sjeveroistočnog smjera te istočni vjetar sa Učke. Prirodni uvjeti Istre pružaju idealno okruženje za tradicionalnu proizvodnju istarskog pršuta, jer zbog utjecaja mediteranske klime izostaju ekstremno niske temperature, a područje hlade česti vjetrovi.";
-        this.text3="Tehnologija Istarskog pršuta Primarna obrada buta je jedinstvena, jer se istarski pršut tradicionalno obrađuje sa zdjeličnim kostima. Na butu ostaju kosti kukovlja, odnosno bočna kost(os ilium), sjedna kost (os ischii) i preponska kost (os pubis),a odstranjuje se samo križna kost (os sacrum) i repni kralješci (vertebrae caudales). Nogica se odstranjuje u skočnom zglobu (articulus tarsi),tako da u vezi sa potkoljenicom (tibia i fibula) ostaje proksimalni red skočnih kosti (talus i calcaneus).S vanjske (lateralne) strane i unutarnje (medialne) strane buta odstranjuje se koža s potkožnim masnim tkivom do visine cca l0 cm proksimalno od skočnog zgloba. Ovako obrađeni butovi imaju karakterističan dugi izgled i otvorenu površinu.";
         this.naslov="Zašto pršut?";
         this.slika=require("../assets/JPEG/isap_prsut.jpg") ;
         }
