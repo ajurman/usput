e22cc29d5f24ae516bd039aa8c7f7909a690b6b9
ajurman <56589117+ajurman@users.noreply.github.com>
2020-01-24 16:41:37 +0100

    Update galerija.vue

--- src/views/galerija.vue
+++ src/views/galerija.vue
@@ -1,17 +1,399 @@
 <template>
-  <div class="about">
-    <h1>Galerija</h1>
-    <div class="container tabs is-large">
+  <div class="about container">
+    <!--<div class="container tabs is-large">
       <ul>
-        <li class="is-active"><a>2019</a></li>
+        <li :class="{'aktivan' : jeLiAktivan}"><a>2019</a></li>
         <li><a>2018</a></li>
         <li><a>2017</a></li>
         <li><a>2016</a></li>
       </ul>
     </div>
+    <div class="container">
+      <div class="columns is-mobile is-multiline">
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+          </figure>
+        </div>
+        
+      </div>
+  </div>
+
+  pokusaj custom tabova
+
+
+  <tabs>
+    <tab name="2019" :selected="true">
+      <div class="container">
+        <div class="columns is-mobile is-multiline">
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+          
+        </div>
+      </div>
+    </tab>
+    <tab name="2018">
+      <h1>How much we do it for</h1>
+    </tab>
+    <tab name="2017">
+      <h1>Why we do it</h1>
+    </tab>
+  </tabs>
+  -->
+  <b-tabs size="is-large">  
+    <b-tab-item label="2019" >
+      <div class="columns is-mobile is-multiline">
+        <div
+          v-for="tab in dve19" 
+          :key="tab.id"
+          class="column">
+          <figure class="image is-128x128" >
+            <img class="" :src="tab.photo" @click="imageModal(tab.photo)">
+          </figure>
+        </div>
+      </div>
+      </b-tab-item>
+    <b-tab-item label="2018" >
+      <div class="columns is-mobile is-multiline">
+        <div
+          v-for="tab in dve18" 
+          :key="tab.id"
+          class="column">
+          <figure class="image is-128x128">
+            <img class="" :src="tab.photo" @click="imageModal(tab.photo)">
+          </figure>
+        </div>
+      </div>
+      </b-tab-item>
+    <b-tab-item label="2017" >
+      <div class="columns is-mobile is-multiline">
+        <div
+          v-for="tab in dve17" 
+          :key="tab.id"
+          class="column">
+          <figure class="image is-128x128">
+            <img class="" :src="tab.photo" @click="imageModal(tab.photo)">
+          </figure>
+        </div>
+      </div>
+      </b-tab-item>
+  </b-tabs>
   </div>
   
 </template>
+<script>
+//import tabs from "../components/tabs"
+//import tab from "../components/tab"
 
-<style scoped>
+export default {
+  data(){
+    return{
+      dve19:[
+        {
+          id:1,
+          photo:require("../assets/dummy.png")
+        },
+        {
+          id:2, 
+          photo:require("../assets/JPEG/isap.jpg")
+        },
+        {
+          id:3,
+          photo:require("../assets/dummy.png")
+        }
+      ],
+      dve18:[
+        {
+          id:1,
+          photo:require("../assets/JPEG/isap.jpg")
+        },
+        {
+          id:2,
+          photo:require("../assets/JPEG/isap.jpg")
+        },
+        {
+          id:3,
+          photo:require("../assets/JPEG/isap.jpg")
+        },
+        {
+          id:4,
+          photo:require("../assets/JPEG/isap.jpg")
+        }
+      ],
+      dve17:[
+        {
+          id:1,
+          photo:require("../assets/JPEG/isap_prsut.jpg")
+        },
+        {
+          id:2,
+          photo:require("../assets/JPEG/isap.jpg")
+        },
+        {
+          id:3,
+          photo:require("../assets/JPEG/isap_prsut.jpg")
+        },
+        {
+          id:4,
+          photo:require("../assets/JPEG/isap_prsut.jpg")
+        },
+        {
+          id:5,
+          photo:require("../assets/JPEG/isap_prsut.jpg")
+        }
+      ]
+    }
+  },
+   methods: {
+      imageModal(arg) {
+          this.$buefy.modal.open(
+            `<img src=${arg}>`)
+      }
+   }
+}
+</script>
+<style lang="scss" scoped>
+.columns{
+  margin-top: 3rem;
+  display: flex;
+  justify-content: center;
+}
+.column{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 256px !important;
+  width: 256px !important;
+  img{
+    cursor: pointer;
+  }
+}
 </style>
